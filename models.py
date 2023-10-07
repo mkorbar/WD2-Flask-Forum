284578aa3696842c4f71fc01e84a5c089671970d
mkorbar <matej.korbar@gmail.com>
2023-10-07 10:05:59 +0200

    Add a forum

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import os
 from sqla_wrapper import SQLAlchemy
+from datetime import datetime
 
 # this connects to a database either on Heroku or on localhost
 db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
@@ -12,3 +13,20 @@ class User(db.Model):
     secret_number = db.Column(db.Integer)
     passwd = db.Column(db.String)
     session_token = db.Column(db.String)
+
+
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    text = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author = db.relationship("User")
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    @classmethod
+    def create(cls, title, text, author):
+        topic = cls(title=title, text=text, author=author)
+        db.add(topic)
+        db.commit()
+
+        return topic

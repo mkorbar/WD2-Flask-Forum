c47dfb1eca1c9d2bf77c71821b8c994438df2b77
mkorbar <matej.korbar@gmail.com>
2023-10-26 16:50:49 +0200

    Basic javascript functionality

---
+++ static/my_test.js
@@ -0,0 +1,66 @@
+
+//let variableName = 'test string in my variable'
+
+// comment in javascript
+
+/*
+multi
+line
+comment
+
+
+
+let numbers_array = [1, 2, 3, 'four', ['five']]
+
+let someObjet = {name: 'Matej', username: 'mkorbar', email: 'matej@korbar.si'}
+
+if (variableName == 'test string') {
+    console.log('the condition was true')
+} else {
+    console.log('condition was false')
+}
+
+/*
+let count = 0
+
+while (count < 10) {
+    console.log('count is ' + count)
+    count ++
+}
+for (let number of numbers_array) {
+    console.log(number)
+}
+console.log('-------------')
+console.log(number)
+
+function sum(x, y){
+    return x + y
+}
+
+
+console.log('10' + 5)
+console.log('10' - 5)
+console.log(100/NaN)
+
+
+
+console.log('10' == 10)
+console.log('10' === 10)
+*/
+
+
+/*
+for(let i = 1; i <= 100; i++) {
+  let out = '';
+  if(i % 3 === 0) {
+    out += 'Fizz';
+  }
+  if(i % 5 === 0) {
+    out += 'Buzz';
+  }
+  if(out === '') {
+    out += i;
+  }
+  console.log(out);
+}
+*/
